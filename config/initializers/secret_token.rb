e3cfea85911dba274f5aa56a6274cff9fb3ed80c
Bernardo Rufino <contato@bernardorufino.com.br>
2013-06-03 21:40:35 -0300

    First commit with dummy views.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortalH8Lab::Application.config.secret_token = '6a8d52ffd84cc837a6a0af71c34bbe16eee44cbb72ef689e14d65b86b29a25f1fbbd7d303615b0a3f75268af1cb8236f050ff2a4d681b20193acf7a8149ceb94'
